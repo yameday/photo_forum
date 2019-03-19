37ee34c6807d93131384f57f52c30ca3e8daa3a0
yameday <yameday@cc.com>
2019-03-19 12:06:55 +0900

    add index action and view for public users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  
+  def index
 
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -25,5 +29,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :intro)
   end
-  
+
 end
