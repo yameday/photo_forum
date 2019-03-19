47523046d1c553dc050f9961b4b69aff946d6019
yameday <yameday@cc.com>
2019-03-19 18:10:55 +0900

    make like as template and add it to user#show page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,7 @@ class FavoritesController < ApplicationController
   @favorite = current_user.favorites.build(like_id: params[:like_id])
 
     if @favorite.save
-      flash[:notice] = "Successfully followed"
+      flash[:notice] = "Successfully liked"
       redirect_back(fallback_location: root_path)
     else
       # 驗證失敗時，Model 將錯誤訊息放在 errors 裡回傳
@@ -19,7 +19,7 @@ class FavoritesController < ApplicationController
     # where 會回傳物件集合(Array)，需要串接可取出單一物件的方法如 first
     @favorite = current_user.favorites.where(like_id: params[:id]).first
     @favorite.destroy
-    flash[:alert] = "favorite destroyed"
+    flash[:alert] = "like destroyed"
     redirect_back(fallback_location: root_path)
   end
 end
