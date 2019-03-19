97bce1c94521d0e54b28ec9702cecd2623f57937
yameday <yameday@cc.com>
2019-03-19 14:09:57 +0900

    set index route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   root "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show, :edit, :update]
 end
